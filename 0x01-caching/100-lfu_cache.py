35fc3219df487b8a5f851766cca86093228f2996
AshTechs <ashbelh@gmail.com>
2024-07-24 16:11:55 -0700

    Done

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -4,9 +4,10 @@
 from collections import defaultdict, OrderedDict
 from base_caching import BaseCaching
 
+
 class LFUCache(BaseCaching):
-    """ Defines a caching system using the Least Frequently Used algorithm
-        with Least Recently Used (LRU) tie-breaking.
+    """ Defines a caching system using the Least Frequently Used (LFU) algorithm
+    with Least Recently Used (LRU) tie-breaking.
     """
     def __init__(self):
         """ Initialize LFUCache """
@@ -15,9 +16,10 @@ class LFUCache(BaseCaching):
         self.order = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        """ Add an item to the cache """
         if key is None or item is None:
             return
+        
         if key in self.cache_data:
             self.cache_data[key] = item
             self.freq[key] += 1
@@ -26,6 +28,7 @@ class LFUCache(BaseCaching):
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 min_freq = min(self.freq.values())
                 lfu_candidates = [k for k, v in self.freq.items() if v == min_freq]
+                
                 if lfu_candidates:
                     lru_key = min(lfu_candidates, key=lambda k: self.order[k])
                     del self.cache_data[lru_key]
@@ -38,10 +41,10 @@ class LFUCache(BaseCaching):
             self.order[key] = None
 
     def get(self, key):
-        """ Get an item by key """
+        """ Retrieve an item by key """
         if key is None or key not in self.cache_data:
             return None
-
+        
         self.freq[key] += 1
         self.order.move_to_end(key)
         return self.cache_data[key]
