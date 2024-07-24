842e603495eb1ca7244404114d42d90ab03b0fc3
AshTechs <ashbelh@gmail.com>
2024-07-24 15:39:39 -0700

    Done

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" LRUCache module
+"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache defines:
+      - a caching system that follows LRU algorithm
+    """
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        If key or item is None, this method should not do anything.
+        If the number of items in self.cache_data is higher than BaseCaching.
+        discard the least recently used item (LRU algorithm) and print DISCARD.
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard_key, _ = self.cache_data.popitem(last=False)
+                print("DISCARD: {}".format(discard_key))
+
+    def get(self, key):
+        """ Get an item by key
+        If key is None or if the key doesn’t exist, return None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
