51f17ed68fc7605eb4f25ec48a3b26a5e4774044
AshTechs <ashbelh@gmail.com>
2024-07-24 16:20:29 -0700

    Done

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -6,14 +6,15 @@ from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ Defines a caching system using the Least Frequently Used (LFU) algorithm
-    with Least Recently Used (LRU) tie-breaking.
+    """ LFUCache class defines a cache with Least Frequently Used eviction policy
+    and Least Recently Used tie-breaking.
     """
     def __init__(self):
         """ Initialize LFUCache """
         super().__init__()
-        self.freq = defaultdict(int)
-        self.order = OrderedDict()
+        self.freq = defaultdict(int)  # Frequency of access
+        self.items = defaultdict(OrderedDict)  # Items at each frequency
+        self.min_freq = 0  # Minimum frequency in the cache
 
     def put(self, key, item):
         """ Add an item to the cache """
@@ -23,29 +24,35 @@ class LFUCache(BaseCaching):
         if key in self.cache_data:
             self.cache_data[key] = item
             self.freq[key] += 1
-            self.order.move_to_end(key)
+            old_freq = self.freq[key] - 1
+            del self.items[old_freq][key]
+            if not self.items[old_freq] and old_freq == self.min_freq:
+                self.min_freq += 1
+            self.items[self.freq[key]][key] = item
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                min_freq = min(self.freq.values())
-                lfu_candidates = ([k for k, v in self.freq.items()
-                                  if v == min_freq])
-                
-                if lfu_candidates:
-                    lru_key = min(lfu_candidates, key=lambda k: self.order[k])
-                    del self.cache_data[lru_key]
-                    del self.freq[lru_key]
-                    self.order.pop(lru_key)
-                    print(f"DISCARD: {lru_key}")
+                key_to_remove, _ = self.items[self.min_freq].popitem(last=False)
+                del self.cache_data[key_to_remove]
+                del self.freq[key_to_remove]
+                print(f"DISCARD: {key_to_remove}")
 
             self.cache_data[key] = item
             self.freq[key] = 1
-            self.order[key] = None
+            self.items[1][key] = item
+            self.min_freq = 1
 
     def get(self, key):
-        """ Retrieve an item by key """
+        """ Retrieve an item from the cache """
         if key is None or key not in self.cache_data:
             return None
         
+        freq = self.freq[key]
         self.freq[key] += 1
-        self.order.move_to_end(key)
+        new_freq = self.freq[key]
+        
+        del self.items[freq][key]
+        if not self.items[freq] and freq == self.min_freq:
+            self.min_freq += 1
+        
+        self.items[new_freq][key] = self.cache_data[key]
         return self.cache_data[key]
