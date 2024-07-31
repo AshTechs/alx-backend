6f0dc8116ccda9d6c2f1a7a5ef69c938d779b7bc
AshTechs <ashbelh@gmail.com>
2024-07-30 19:50:25 -0700

    Done

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Basic Flask app for displaying 'Hello world' with Babel integration."""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configuration for Babel."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages."""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Route for the home page."""
+    return render_template(
+                          ('4-index.html', home_title=_('home_title'),
+                              home_header=_('home_header')))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
