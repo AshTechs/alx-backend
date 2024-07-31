e22a7ffb224f9178d355526cf7a9e80fab62267f
AshTechs <ashbelh@gmail.com>
2024-07-30 19:13:22 -0700

    Corrected

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Basic Flask app for displaying 'Hello world' with Babel integration."""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration for Babel."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Route for the home page."""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
