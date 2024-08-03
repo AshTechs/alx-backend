1e52cef936d1292a6e21b8f05fa6342fb8d1e12a
AshTechs <ashbelh@gmail.com>
2024-08-03 02:42:49 +0000

    Done

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3
-"""Basic Flask app for displaying 'Hello world' with Babel integration."""
+"""
+Basic Flask app for displaying 'Hello world' with Babel integration.
 
-from flask import Flask, render_template, request, g
+This module sets up a Flask application with Babel for language and timezone
+support. The app has a single route that displays a localized 'Hello world'
+message. It also includes functions to determine the appropriate locale and 
+timezone based on the request.
+
+Classes:
+    Config: Configuration class for Babel.
+
+Functions:
+    get_locale: Determines the best match with the supported languages.
+    get_timezone: Determines the correct timezone based on the request.
+    index: Route for the home page.
+
+Usage:
+    Run this module to start the Flask application.
+"""
+
+from flask import Flask, render_template, request, Response
 from flask_babel import Babel, _
 import pytz
 from pytz.exceptions import UnknownTimeZoneError
-from datetime import datetime
+from typing import Optional
 
 
 class Config:
-    """Configuration for Babel."""
+    """
+    Configuration for Babel.
+
+    Attributes:
+        LANGUAGES (list): Supported languages for the application.
+        BABEL_DEFAULT_LOCALE (str): Default locale for the application.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the application.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -19,45 +44,38 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
-def get_user():
-    """Retrieve user based on the login_as URL parameter."""
-    user_id = request.args.get('login_as')
-    if user_id:
-        return users.get(int(user_id))
-    return None
-
-
-@app.before_request
-def before_request():
-    """Set user on Flask global object before each request."""
-    g.user = get_user()
-
 
 @babel.localeselector
-def get_locale():
-    """Determine the best match with our supported languages."""
+def get_locale() -> Optional[str]:
+    """
+    Determine the best match with our supported languages.
+
+    This function checks the 'locale' parameter in the URL. If it's present and
+    valid, it returns that locale. Otherwise, it uses the best match from the
+    'Accept-Language' header in the request.
+
+    Returns:
+        str: The best matching locale or None if no match is found.
+    """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
-    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
-        return g.user.get('locale')
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @babel.timezoneselector
-def get_timezone():
-    """Determine the correct timezone."""
+def get_timezone() -> str:
+    """
+    Determine the correct timezone based on the request.
+
+    This function checks the 'timezone' parameter in the URL. If it's present &
+    valid, it returns that timezone. Otherwise, it defaults to the configured
+    default timezone.
+
+    Returns:
+        str: Determined or the default timezone if no valid timezone is found.
+    """
     timezone = request.args.get('timezone')
-    if g.user and g.user.get('timezone'):
-        timezone = g.user.get('timezone')
     if timezone:
         try:
             return pytz.timezone(timezone).zone
@@ -67,29 +85,22 @@ def get_timezone():
 
 
 @app.route('/')
-def index():
-    """Route for the home page."""
+def index() -> Response:
+    """
+    Route for the home page.
+
+    This function handles the root URL ('/') and renders the '3-index.html'
+    template with localized strings for the title and header.
+
+    Returns:
+        Response: The Flask response object with the rendered template.
+    """
     home_title = _('home_title')
     home_header = _('home_header')
-
-    # Determine current time
-    user_timezone = pytz.timezone(get_timezone())
-    current_time = (
-                datetime.now(user_timezone).strftime('%b %d, %Y, %I:%M:%S %p'))
-
-    if g.user:
-        welcome_message = _('logged_in_as', username=g.user['name'])
-    else:
-        welcome_message = _('not_logged_in')
-
-    current_time_message = _('current_time_is', current_time=current_time)
-
     return render_template(
         '3-index.html',
         home_title=home_title,
-        home_header=home_header,
-        welcome_message=welcome_message,
-        current_time_message=current_time_message
+        home_header=home_header
     )
 
 
